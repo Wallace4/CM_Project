64886940b748ff346287bab3e541142445c3585f
Wallace4 <mikde4@gmail.com>
2021-11-20 21:32:08 +0100

    Added dual and general algorithm

--- active_set.py
+++ active_set.py
@@ -172,6 +172,23 @@ class quadratic_problem:
         self.logger.info(f"z[n] + r[n] >= 0: {condition_5}") 
         assert np.allclose(condition_5, True, rtol=self.tol), condition_5
         return True
+
+    def general_active_set (self):
+        pdb = np.array([False, False])
+        try:
+            pdb[0] = self.test_primal_feasible()
+            self.logger.info(f"The current set of variables is feasible for a Primal algorithm")
+            self.primal_active_set()
+        except AssertionError as err:
+            self.logger.error(f"The current set of variables is not feasible for a Priaml algorithm")
+        try:
+            pdb[1] = self.test_dual_feasible()
+            self.logger.info(f"The current set of variables is feasible for a Dual algorithm")
+            self.dual_active_set()
+        except AssertionError as err:
+            self.logger.error(f"The current set of variables is not feasible for a Dual algorithm")
+        if not any(pdb):
+            self.logger.error(f"The current set of variables is not feasible for any algorithm")
     
     def primal_active_set(self):
         """! Function that start the active set Primal Algorithm
@@ -355,7 +372,7 @@ class quadratic_problem:
         self.x[self.B] += alpha * self.dx[self.B]
         self.y    += alpha * self.dy
         self.z[l] += alpha * self.dz[l]
-        self.z[self.N] += alpha * self.dz[N]
+        self.z[self.N] += alpha * self.dz[self.N]
         
         if self.z[l] + self.r[l] < 0:
             self.logger.info(f"k: {k}")
@@ -369,8 +386,205 @@ class quadratic_problem:
         self.logger.info(f"N:\n{self.N}")
         return
 
-    # kind of a mess but ok
+    def dual_active_set(self):
+        """! Function that start the active set Dual Algorithm
 
+        @return x vector R(n) with the optimal solution
+        @return y vector R(m) with the optimal solution
+        @return z vector R(n) with the optimal solution
+        """
+        self.logger.info("-"*20)
+        self.logger.info(f"Starting the resolution of the Dual Problem of the Active Sets")
+
+        res = self.test_dual_feasible()
+        
+        # --------------- Inizio loop principale
+        while True:
+            l_list = np.argwhere((self.x - self.q) < 0)
+            self.logger.info(f"The indexes that violate the constrains are: {l_list.flatten()}")
+            if l_list.size == 0:
+                self.logger.info(f"The dual algorith just terminated its course. The solutions are as follows:")
+                self.logger.info(f"x:\n{self.x}")
+                self.logger.info(f"y:\n{self.y}")
+                self.logger.info(f"z:\n{self.z}")
+                return (
+                    self.x,
+                    self.y,
+                    self.z,
+                )  # non si può fare un passo, aka siamo arrivati alla nostra soluzione ottima
+            l = l_list[0]
+            self.B[l] = False  # prendo il primo elemento di l e lo levo da N.
+            self.dual_base(l)
+            self.reset_deltas()
+            while (self.x[l] + self.q[l]) < 0:
+                input("premi per continuare...")
+                self.dual_intermediate(l)
+                self.reset_deltas()
+            self.N[l] = True
+
+    def dual_base(self, l):
+        """! Function that do the base step of the Dual problem
+
+        @param l the index that violate the constrain
+        """
+        self.logger.info(f"-"*20)
+        self.logger.info(f"Base step of the Dual problem, done with the index: {l}")
+        
+        self.dz[l] = 1
+        B_size = np.sum((self.B))
+
+        self.logger.info(f"Hll:\n{self.H[l, l]}")
+        self.logger.info(f"HBl:\n{self.H[self.B][:, l]}")
+        self.logger.info(f"Hbb:\n{self.H[self.B][:, self.B]}")
+        self.logger.info(f"Al:\n{self.A[:, l]}")
+        self.logger.info(f"Ab:\n{self.A[:, self.B]}")
+        self.logger.info(f"M:\n{self.M}")
+        K_I = np.block([
+            [self.H[l, l],         self.H[self.B, l].T,       self.A[:, l].T     ],
+            [self.H[self.B][:, l], self.H[self.B][:, self.B], self.A[:, self.B].T],
+            [self.A[:, l],         self.A[:, self.B],         -self.M]
+        ])
+        self.logger.info(f"K_I:\n{K_I}")
+        
+        tmp_b = np.concatenate((
+            np.ones(1),
+            np.zeros(B_size + self.y.size))
+        )
+        self.logger.info(f"b:\n{tmp_b}")
+        
+        tmp_sol = np.linalg.solve(K_I, tmp_b).reshape((1+B_size+self.y.size,))
+        self.logger.info(f"sol:\n{tmp_sol}")
+        # robo da risolvere
+        self.dx[l], self.dx[self.B], self.dy[:] = tmp_sol[0], tmp_sol[1: B_size +1], -tmp_sol[B_size + 1:]
+        self.logger.info(f"delta x:\n{self.dx}")
+        self.logger.info(f"delta y:\n{self.dy}")
+
+        self.dz[self.N] = (
+              self.H[self.N, l]           * self.dx[l] #scalare 
+            + self.H[self.B][:, self.N].T @ self.dx[self.B] #moltiplicazione #Nx#B per #Bx1
+            - self.A[:, self.N].T         @ self.dy[:] # moltiplicazione #Nxm per #mx1
+        )
+        self.logger.info(f"delta z\n{self.dz}")
+        
+        alpha_opt = np.inf if np.allclose(self.dx[l], 0, rtol=self.tol) else -(self.x[l] + self.q[l])/self.dx[l]
+        min_mask = ( self.dz < 0 )
+        to_min = self.r + self.z
+        to_min[~min_mask] = np.inf
+        to_min[min_mask] = to_min[min_mask]/-self.dz[min_mask]
+        print(f"min_mask: {min_mask}")
+        print(f"z: {self.x[min_mask]} r: {self.q[min_mask]}")
+        self.logger.info(f"to_min:\n{to_min}")
+        k = np.argmin(to_min)
+        alpha_max = to_min[k]
+
+        alpha = min(alpha_opt, alpha_max)
+        self.logger.info(f"alpha = min ( opt = {alpha_opt}; max = {alpha_max});")
+
+        if np.isinf(alpha):
+            self.logger.exception(f"Dual is Unbounded (Primal is unfeasible")
+            raise Exception("Dual is Unboundend (Primal is unfeasible)")
+        
+        self.x[l] += alpha * self.dx[l]
+        self.x[self.B] += alpha * self.dx[self.B]
+        self.y    += alpha * self.dy
+        self.z[l] += alpha * self.dz[l]
+        self.z[self.N] += alpha * self.dz[self.N]
+        
+        if self.x[l] + self.q[l] < 0:
+            self.logger.info(f"k: {k}")
+            self.B[k] = True
+            self.N[k] = False
+        
+        self.logger.info(f"x:\n{self.x}")
+        self.logger.info(f"y:\n{self.y}")
+        self.logger.info(f"z:\n{self.z}")
+        self.logger.info(f"B:\n{self.B}")
+        self.logger.info(f"N:\n{self.N}")
+        return
+
+    def dual_intermediate(self, l):
+        """! Function that do the intermediate iteration of the Dual problem
+
+        @param l the index that violate the constrain
+        """
+        self.logger.info("-"*20)
+        self.logger.info(f"Intermadiate iteration of the Dual Problem, with the index: {l}")
+        
+        self.dx[l] = 1.
+        B_size = np.sum((self.B))
+        
+        self.logger.info(f"Hbb:\n{self.H[self.B, :][:, self.B]}")
+        self.logger.info(f"Ab:\n{self.A[:, self.B]}")
+        self.logger.info(f"M:\n{self.M}")
+        K_I = np.block ([
+            [self.H[self.B, :][:, self.B], self.A[:, self.B].T],
+            [self.A[:, self.B],           -self.M             ],
+        ])
+        self.logger.info(f"K_I:\n{K_I}")
+
+        self.logger.info(f"Hb:\n{self.H[self.B][:, l]}")
+        self.logger.info(f"Al:\n{self.A[:, l]}")
+        tmp_b = -np.concatenate(
+            (self.H[self.B][:, l], self.A[:, l]),
+            axis=0
+        )
+        self.logger.info(f"b:\n{tmp_b}")
+        
+        tmp_sol = np.linalg.solve(K_I, tmp_b).reshape((B_size + self.y.size,)) #da cambiare
+        self.logger.info(f"sol:\n{tmp_sol}")
+        
+        self.dx[self.B], self.dy[:] = tmp_sol[:B_size], -tmp_sol[B_size:]
+        self.logger.info(f"delta x:\n{self.dx}")
+        self.logger.info(f"delta y:\n{self.dy}")
+
+        self.dz[self.N] = (
+              self.H[self.N, l]           * self.dx[l] #qui * va bene perché delta_x_l è uno scalare
+            + self.H[self.B][:, self.N].T @ self.dx[self.B] #qui usiamo matmul perché è la moltiplicazione di una matrice #Nx#B per un vettore #Bx1
+            - self.A[:, self.N].T         @ self.dy #come sopra, #Nxm per mx1
+        )
+        self.dz[l] = (
+              self.H[l, l]           * self.dx[l] #scalare
+            + self.H[self.B][:, l].T @ self.dx[self.B] # qui è un vettore 1x#B per #B
+            - self.A[:, l].T         @ self.dy # qui è 1xm per mx1
+        )
+        self.logger.info(f"delta z:\n{self.dz}")
+
+        alpha_opt = -(self.x[l] + self.q[l])
+
+        min_mask = ( self.dx < 0 )
+        to_min = self.z + self.r
+        to_min[~min_mask] = np.inf
+        to_min[min_mask] = to_min[min_mask]/-self.dx[min_mask]
+        print(f"min_mask: {min_mask}")
+        print(f"x: {self.x[min_mask]} q: {self.q[min_mask]}")
+        self.logger.info(f"to_min:\n{to_min}")
+        k = np.argmin(to_min)
+        alpha_max = to_min[k]
+
+        alpha = min(alpha_opt, alpha_max)
+        self.logger.info(f"alpha = min ( opt: {alpha_opt}; max: {alpha_max});")
+        
+        if np.isinf(alpha):
+            self.logger.exception(f"Dual is Unbounded (Primal is unfeasible")
+            raise Exception("Dual is Unboundend (Primal is unfeasible)")  # il problema è impraticabile
+
+        self.x[l] += alpha * self.dx[l]
+        self.x[self.B] += alpha * self.dx[self.B]
+        self.y    += alpha * self.dy
+        self.z[l] += alpha * self.dz[l]
+        self.z[self.N] += alpha * self.dz[self.N]
+        
+        if self.x[l] + self.q[l] < 0: #effettivamente anche alpha == alpha_max ha senso
+            self.logger.info(f"k: {k}")
+            self.B[k] = True
+            self.N[k] = False
+        
+        self.logger.info(f"x:\n{self.x}")
+        self.logger.info(f"y:\n{self.y}")
+        self.logger.info(f"z:\n{self.z}")
+        self.logger.info(f"B:\n{self.B}")
+        self.logger.info(f"N:\n{self.N}")
+        return
 
 if __name__ == "__main__":
     n, m = 4, 6
@@ -380,18 +594,20 @@ if __name__ == "__main__":
     M = np.eye(m, m)
     H = np.eye(n, n)
     
-    b = np.array([ 1., 1., 2., 2., 0., 0.])
-    c = np.array([-1., 3., 1., 1.])
+    b = np.array([-1.,-1., 0., 0., 0., 0.])
+    c = np.array([ 1., 1., 1., 1.])
     qp = quadratic_problem (A, b, c, H, M, verbose = True)
 
-    x = np.array([1., 0., 0., 0.])
-    y = np.array([0., 1., 2., 2., 0., 0.])
-    z = np.array([0., 0.,-1.,-1.])
+    x = np.array([-1.,-1., 0., 0.])
+    y = np.array([ 0., 0., 0., 0., 0., 0.])
+    z = np.array([ 0., 0., 1., 1.])
     B = np.array([True, True, False, False])
     N = ~B
     
     qp.set_initial_solution(x, y, z)
     qp.set_initial_active_set(B, N)
-    qp.primal_active_set()
+    #qp.primal_active_set()
+    #qp.dual_active_set()
+    qp.general_active_set()
     print(qp.get_solution())
     pass
