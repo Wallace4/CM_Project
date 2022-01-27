4a50280af999cdf81bc557adabb7c3f12663a56c
Wallace4 <mikde4@gmail.com>
2022-01-27 16:02:51 +0100

    added a test to see if the solution is equal to the one of a real qp solver (it is)

--- min_flow.py
+++ min_flow.py
@@ -1,4 +1,6 @@
 import numpy as np
+import quadprog
+import cvxopt
 from active_set import quadratic_problem
 
 class min_flow (quadratic_problem):
@@ -19,7 +21,7 @@ E = np.array([[-1.,-1., 0., 0., 0., 0., 0.],
               [ 0., 0., 0., 0., 1., 0.,-1.]])
 print(np.linalg.matrix_rank(E))
 
-b = np.array([-1., 0., 0., 0., 0.])
+b = np.array([-2., 0., 0., 0., 0.])
 q = 1 * np.ones(n)
 u = 3 * np.ones(n)
 
@@ -29,4 +31,19 @@ B = np.array([True, False, True, False, True, False, True])
 N = ~B
 
 #qp.set_initial_active_set_from_factorization()
-qp.solve()
+try:
+    qp.solve()
+except:
+    print("oh no")
+
+#print(quadprog.solve_qp(Q, q, E, b, m, True))
+ineqm = np.block([ [-np.eye(n)], [np.eye(n)] ])
+ineqv = np.concatenate((np.zeros(n), u))
+print(ineqv)
+sol = cvxopt.solvers.qp(cvxopt.matrix(Q), cvxopt.matrix(q),
+                        cvxopt.matrix(ineqm), cvxopt.matrix(ineqv),
+                        cvxopt.matrix(E), cvxopt.matrix(b))
+
+print (f"x: {sol['x']}; y: {sol['y']}; z: {sol['z']}; s:{sol['s']}")
+print (f"primal obj = {sol['primal objective']}")
+print (f"dual obj = {sol['dual objective']}")
